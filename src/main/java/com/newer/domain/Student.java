aa78f9d8883aff351f91829233dc6127167145fb
RowlingPan <714229650@qq.com>
2019-04-02 15:02:20 +0800

    学生类被修改了

--- src/main/java/com/newer/domain/Student.java
+++ src/main/java/com/newer/domain/Student.java
@@ -4,4 +4,14 @@ import java.io.Serializable;
 
 public class Student implements Serializable {
      private Integer sid;
+
+     private String sName;
+
+     public Integer getSid() {
+          return sid;
+     }
+
+     public void setSid(Integer sid) {
+          this.sid = sid;
+     }
 }
